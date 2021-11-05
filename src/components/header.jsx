e64d0d3031d33b4a6be712fa289cea1bc4ed471c
qualley <matt.qualley93@gmail.com>
2021-11-05 01:23:42 -0400

    Updated header section styles

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,25 +2,33 @@ import '../styles/header.css'
 
 function Header() {
   return (
-    <div className="header_main">
+    <div className="header_container">
 
       <div className='header_top'>
-        <p>Dazed</p>
-        <p>Dispensaries</p>
-        <p>Deals</p>
-        <p>Strains</p>
-        <p>Priduct</p>
-        <button>Login</button>
+        <div className="links">
+          <img src="/img/dazed-logo.svg" alt="dazedlogo" class="logo"/>
+          <p>Dazed</p>
+          <p>Dispensaries</p>
+          <p>Deals</p>
+          <p>Strains</p>
+          <p>Priduct</p>
+        </div>
+        <button><img src="/img/account_circle.svg" alt="user icon" class="icon"/>Login</button>
       </div>
 
       <div className='header_slogan'>
-        <p>Find weed, get dazed, today</p>
+        <h1>Welcome to Dazed, <br/> where buying weed is made simple.</h1>
       </div>
 
       <div className='header_search'>
-        <input className='header_search_input' placeholder='Search despensaries, products,strains and more' type="search"></input>
+        <img src="/img/search.svg" alt="search icon" class="logo" class="search"/>
+        <input className='header_search_input' placeholder='Search despensaries, products,strains and more' type="search">
+        </input>
         <button>Search</button>
       </div>
+
+      <div className="header_bg-img"></div>
+      <img src="/img/z.svg" alt="" class="z" />
     </div>
   );
 }
