4dc4b64f0eebf594d241de3f7df77de0e1c7c11d
Carson0350 <carson.workflow@gmail.com>
2021-11-06 10:05:08 -0600

    welcome section done

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,27 +1,38 @@
 import '../styles/welcomeStyles.css'
+import { Grid } from 'react-bootstrap-icons'
 
 function Welcome() {
   return (
     <div className='welcome_main'>
-      <div className='welcome_message'>
-        <h3>Welcome to Dazed</h3>
-        <p className='welcome_intro'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
+      <grid className='grid'>
 
-      <div className='statements'>
-        <p>image</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
+        <div className='welcome_main'>
+          <div className="title">
+            <h2 class="section_title">Welcome to dazed</h2>
+            <p>Dazed is your one stop shop for access to top of the line barnds, deals , and delivery</p>
+          </div>
+        </div>
 
-      <div className='statements'>
-        <p>image</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
+        <div className="welcome_card">
+          <img src="/img/community.svg" alt="" />
+          <h2>Dazed Community</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </div>
 
-      <div className='statements'>
-        <p>image</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
+        <div className="welcome_card">
+          <img src="/img/delivery.svg" alt="" />
+          <h2>Dazed Community</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </div>
+
+        <div className="welcome_card">
+          <img src="/img/snap.png" alt="" />
+          <h2>Dazed Community</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </div>
+
+
+      </grid>
     </div>
   )
 }
